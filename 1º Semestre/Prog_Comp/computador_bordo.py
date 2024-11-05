8860a8276e38039989a075c880abde789304582c
José Bezerra <50915483+jbezerrafilho@users.noreply.github.com>
2024-11-05 20:26:50 -0300

    Add computador_bordo

---
+++ 1º Semestre/Prog_Comp/computador_bordo.py
@@ -0,0 +1,8 @@
+# Autor: José Bezerra
+
+tempo = int(input('Tempo gasto de viagem? '))
+consumo_litros = float(input('Consumo em litros? '))
+preco_gasolina = float(input('Preço do litro de gasolina? '))
+distancia = float(input('Distancia percorrida? ))
+
+#Converter o tempo em minutos em horas
