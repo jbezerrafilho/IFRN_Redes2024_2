3034738ab870cbed2d8de590aac2f39b2ff051ca
José Bezerra <50915483+jbezerrafilho@users.noreply.github.com>
2024-11-21 07:19:12 -0300

    Update q3.py

--- 1º Semestre/Progamação_Computadores/AvaliativaComandoCondicional/q3.py
+++ 1º Semestre/Progamação_Computadores/AvaliativaComandoCondicional/q3.py
@@ -7,54 +7,65 @@ try:
     inicio = 1
     fim = 100
 
+    # Right refere-se ao acerto da tentativa do usuário
+    right = False
+    qtd_tentativas = 0
+    
+    
     #Tentativa 1
-    msg = f'Qual o número sorteado no intervalo de {inicio} a {fim}? '
-    tentativa = int(input(msg))
-    if tentativa == numero_sorteado:
-        print(f'Você acertou o número secreto {numero_sorteado}!')
-    elif tentativa < numero_sorteado:
-        inicio = tentativa + 1
-        fim = 100
-    else:
-        inicio = 0
-        fim = tentativa - 1
+    if not right:
+        msg = f'Qual o número sorteado no intervalo de {inicio} a {fim}? '
+        tentativa = int(input(msg))
+        qtd_tentativas += 1
+        if tentativa == numero_sorteado:
+            print(f'Você acertou! O número secreto é {numero_sorteado}!')
+            right = True
+        elif tentativa < numero_sorteado:
+            inicio = tentativa + 1
+        elif tentativa > numero_sorteado:
+            fim = tentativa - 1
 
     #Tentativa 2
-    msg = f'Qual o número sorteado no intervalo de {inicio} a {fim}? '
-    tentativa = int(input(msg))
-    if tentativa == numero_sorteado:
-        print(f'Você acertou o número secreto {numero_sorteado}!')
-    elif tentativa < numero_sorteado:
-        inicio = tentativa + 1
-        fim = 100
-    else:
-        inicio = 0
-        fim = tentativa - 1
+    if not right:
+        msg = f'Qual o número sorteado no intervalo de {inicio} a {fim}? '
+        tentativa = int(input(msg))
+        qtd_tentativas += 1
+        if tentativa == numero_sorteado:
+            print(f'Você acertou! O número secreto é {numero_sorteado}!')
+            right = True
+        elif tentativa < numero_sorteado:
+            inicio = tentativa + 1
+        elif tentativa > numero_sorteado:
+            fim = tentativa - 1
 
     #Tentativa 3    
-    msg = f'Qual o número sorteado no intervalo de {inicio} a {fim}? '
-    tentativa = int(input(msg))
-    if tentativa == numero_sorteado:
-        print(f'Você acertou o número secreto {numero_sorteado}!')
-    elif tentativa < numero_sorteado:
-        inicio = tentativa + 1
-        fim = 100
-    else:
-        inicio = 0
-        fim = tentativa - 1
-    
+    if not right:
+        msg = f'Qual o número sorteado no intervalo de {inicio} a {fim}? '
+        tentativa = int(input(msg))
+        qtd_tentativas += 1
+        if tentativa == numero_sorteado:
+            print(f'Você acertou! O número secreto é {numero_sorteado}!')
+            right = True
+        elif tentativa < numero_sorteado:
+            inicio = tentativa + 1
+        elif tentativa > numero_sorteado:
+            fim = tentativa - 1
+
     #Tentativa 4    
-    msg = f'Qual o número sorteado no intervalo de {inicio} a {fim}? '
-    tentativa = int(input(msg))
-    if tentativa == numero_sorteado:
-        print(f'Você acertou o número secreto {numero_sorteado}!')
-    elif tentativa < numero_sorteado:
-        inicio = tentativa + 1
-        fim = 100
-    else:
-        inicio = 0
-        fim = tentativa - 1
-    print('Que pena, Game Over!!')
+    if not right:
+        msg = f'Qual o número sorteado no intervalo de {inicio} a {fim}? '
+        tentativa = int(input(msg))
+        qtd_tentativas += 1
+        if tentativa == numero_sorteado:
+            print(f'Você acertou! O número secreto é {numero_sorteado}!')
+            right = True
+        elif tentativa < numero_sorteado:
+            inicio = tentativa + 1
+        elif tentativa > numero_sorteado:
+            fim = tentativa - 1
+    if qtd_tentativas == 4:
+        print('Tentativas esgotadas!')
+        
 except ValueError as e:
     print(e)
     print('Digite um número válido!')
