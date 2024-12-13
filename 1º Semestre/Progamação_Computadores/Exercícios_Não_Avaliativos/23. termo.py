5a20d953b8f41b8168aef9f2e9d4f0dea458dce4
José Bezerra <jbezerra.filho@outlook.com>
2024-12-13 18:17:36 -0300

    Change file

--- 1º Semestre/Progamação_Computadores/Exercícios_Não_Avaliativos/23. termo.py
+++ 1º Semestre/Progamação_Computadores/Exercícios_Não_Avaliativos/23. termo.py
@@ -1,21 +1,28 @@
-import random
-palavras = ['CENOURA', 'TOMATE', 'CEBOLA', 'LARANJA']
+palavras = ['LAPIS', 'TEXTO', 'LINHAS', 'CORES']
 
-sorteada = random.choice(palavras)
+sorteada = 'LAPIS'
 atual = "_" * len(sorteada)
-tentativas = 2 * len(sorteada)
+tentativas = 6
+GREEN = "\033[0;32m"
+BLUE  = "\033[1;34m"
+RED   = "\033[1;31m"  
 
 while (atual != sorteada) and (tentativas > 0):
-    letra = input('Digite uma letra: ')
+    palavra = input('Digite uma palavra: ').upper()
     # A variável 'nova' foi criada porque não podemos atualizar atual 
     # Lmebre-se que String é invariável
-    nova = ''
 
-    for posicao in range(len(sorteada)):
-        if sorteada[posicao] == letra.upper():
-            nova = nova + sorteada[posicao]
-        else:
-            nova = nova + atual[posicao]
+    nova = ''
+    if palavra == sorteada:
+        nova = sorteada
+    else:
+        for posicao in range(len(sorteada)):
+            if sorteada[posicao] == palavra[posicao]:
+                nova = nova + sorteada[posicao]             
+            elif palavra[posicao] in sorteada:
+                nova = nova + palavra[posicao]
+            else:
+                nova = nova + atual[posicao]
     print(nova)
     atual = nova
     tentativas -= 1
